aae1136f15cc7f06cd30f4674dd15a8d6af28d50
Christer Warén <cwchristerw@gmail.com>
2020-08-16 13:23:30 +0300

    Changed Docker

--- contrib/docker/entrypoint.sh
+++ contrib/docker/entrypoint.sh
@@ -3,6 +3,8 @@
 set -e
 
 cron
-cd /src/yggdrasil-map/web/ && python updateGraph.py
+#cd /src/yggdrasil-map/scripts/ && python crawl-dht.py
+#cd /src/yggdrasil-map/web/ && python updateGraph.py
+cd /src/yggdrasil-map/web/static/ && wget http://[21f:dd73:7cdb:773b:a924:7ec0:800b:221e]/static/graph.json
 python /src/yggdrasil-map/web/web.py --host $HOST --port $PORT
 exit $?
